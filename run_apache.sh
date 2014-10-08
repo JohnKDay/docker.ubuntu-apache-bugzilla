2ac8a8de61855cce6a6cfa6a887b7340ed95f058
Jindrich Vimr <jvimr@softeu.com>
2014-10-08 17:50:49 +0200

    db login data passed to the bugzilla

--- run_apache.sh
+++ run_apache.sh
@@ -13,7 +13,9 @@ echo "ServerName $SERVER_NAME" >> /etc/apache2/conf-enabled/servername.conf
 
 echo "\$db_host = \"$MYSQL_PORT_3306_TCP_ADDR\";" >> /var/www/html/bugzilla/localconfig
 echo "\$db_port = \"$MYSQL_PORT_3306_TCP_PORT\";" >> /var/www/html/bugzilla/localconfig
-
+echo "\$db_user = \"$MYSQL_USER\"; " >> /var/www/html/bugzilla/localconfig
+echo "\$db_name = \"$MYSQL_DB\"; " >> /var/www/html/bugzilla/localconfig
+echo "\$db_pass = \"$MYSQL_PASS\"; "  >> /var/www/html/bugzilla/localconfig
 
 
 /etc/init.d/apache2 start && \
