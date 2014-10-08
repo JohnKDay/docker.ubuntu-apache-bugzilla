46374aae5a1085f55f64e49013b6a8e6f3c0a07c
Jindrich Vimr <jvimr@softeu.com>
2014-10-08 17:44:02 +0200

    initial commit

---
+++ run_apache.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+set |grep '_PORT_' |tr  '=' ' ' |sed 's/^/SetEnv /' > /etc/apache2/conf-enabled/docker-vars.conf
+set |grep '_ENV_' |tr  '=' ' ' |sed 's/^/SetEnv /' >> /etc/apache2/conf-enabled/docker-vars.conf
+set |grep 'SERVER_NAME' |tr  '=' ' ' |sed 's/^/SetEnv /' >> /etc/apache2/conf-enabled/docker-vars.conf
+
+set |grep '_PORT_'  |sed 's/^/export /'>> /etc/apache2/envvars
+
+echo "export SERVER_NAME=$SERVERNAME" >> /etc/apache2/envvars
+
+echo "ServerName $SERVER_NAME" >> /etc/apache2/conf-enabled/servername.conf
+
+echo "\$db_host = \"$MYSQL_PORT_3306_TCP_ADDR\";" >> /var/www/html/bugzilla/localconfig
+echo "\$db_port = \"$MYSQL_PORT_3306_TCP_PORT\";" >> /var/www/html/bugzilla/localconfig
+
+
+
+/etc/init.d/apache2 start && \
+tail -F /var/log/apache2/*log
+
+#service rsyslog start && \
+#service postfix start && \
+#tail -F /var/log/mail.log
