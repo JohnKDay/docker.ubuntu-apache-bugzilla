5b6a012b35be8b924cad4b2a97048263a8be8d3f
Jindrich Vimr <jvimr@softeu.com>
2014-10-08 18:15:10 +0200

    fix

--- run_apache.sh
+++ run_apache.sh
@@ -7,9 +7,9 @@ set |grep 'SERVER_NAME' |tr  '=' ' ' |sed 's/^/SetEnv /' >> /etc/apache2/conf-en
 
 set |grep '_PORT_'  |sed 's/^/export /'>> /etc/apache2/envvars
 
-echo "export SERVER_NAME=$SERVERNAME" >> /etc/apache2/envvars
+echo "export SERVER_NAME=$SERVER_NAME" >> /etc/apache2/envvars
 
-echo "ServerName $SERVER_NAME" >> /etc/apache2/conf-enabled/servername.conf
+echo "ServerName $SERVER_NAME" > /etc/apache2/conf-enabled/servername.conf
 
 echo "\$db_host = \"$MYSQL_PORT_3306_TCP_ADDR\";" >> /var/www/html/bugzilla/localconfig
 echo "\$db_port = \"$MYSQL_PORT_3306_TCP_PORT\";" >> /var/www/html/bugzilla/localconfig
